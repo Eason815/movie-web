0ff967262e8caa8d692a56ba8568025906b1c4a7
Eason815 <806183233@qq.com>
2024-06-01 16:18:20 +0800

    美化搜索功能

--- src/main/java/cn/edu/scnu/controller/SearchController.java
+++ src/main/java/cn/edu/scnu/controller/SearchController.java
@@ -1,4 +1,37 @@
 package cn.edu.scnu.controller;
 
+import cn.edu.scnu.entity.Movie;
+import cn.edu.scnu.entity.Staff;
+import cn.edu.scnu.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
 public class SearchController {
+
+    @Autowired
+    private MovieService movieService;
+
+//    @RequestMapping("/search")
+//    public String toSearchPage(){
+//        return "search";
+//    }
+
+    @GetMapping("/search")
+    public String index(@RequestParam(name = "name", required = false) String name, Model model) {
+        List<Movie> movies = movieService.getMoviesByActorName(name);
+        List<Movie> movies2 = movieService.getMoviesByDirectorName(name);
+        // 合并两个list
+        movies.addAll(movies2);
+
+        model.addAttribute("movies", movies);
+        model.addAttribute("name", name);
+        return "search";
+    }
 }
+
